f709020b7e772572380d1a161001522a36f7f739
eusouodaniel <danielrodriguesdrs331@gmail.com>
2023-06-01 21:02:35 -0300

    new: primeira aula de js

---
+++ 1-primeiro-projeto/js/index.js
@@ -0,0 +1,24 @@
+const btn = document.getElementById("btnAqui");
+
+btn.addEventListener('dblclick', () => {
+    alert('clicou mesmo')
+})
+
+// let nomes = [{
+//     nome: 'Matheus',
+//     curso: 'JS'
+// },{
+//     nome: 'Laura',
+//     curso: 'JS'
+// },{
+//     nome: 'Rohy',
+//     curso: 'JS'
+// }];
+
+// nomes.forEach((nome) => {
+//     alert(nome.nome);
+// })
+
+// for (let x = 0; x < array.length; x++) {
+//     alert(array[x].nome);
+// }
