acda115a42067950e2324b0fa84775d9d74b9f39
eusouodaniel <danielrodriguesdrs331@gmail.com>
2023-06-12 20:40:57 -0300

    new: lesson 12-06

---
+++ 3-terceiro-projeto/index.js
@@ -0,0 +1,20 @@
+function listarPessoas() {
+  let pessoas = [{
+    nome: 'Henrique',
+    sobrenome: 'Nunes',
+    anoDeNascimento: 1995     
+  },
+  {
+    nome: 'Rohy',
+    sobrenome: '',
+    anoDeNascimento: 1998   
+  },
+  {
+    nome: 'Ricardo',
+    sobrenome: 'Pina',
+    anoDeNascimento: 1996
+  }];
+  return pessoas;
+}
+
+const pessoas = listarPessoas();
