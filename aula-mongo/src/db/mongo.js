2acaa8f834b2d87caa0ccc6b5f3263b44231bb87
eusouodaniel <danielrodriguesdrs331@gmail.com>
2023-07-18 20:58:37 -0300

    new: aula 18-07

---
+++ aula-mongo/src/db/mongo.js
@@ -0,0 +1,14 @@
+const { MongoClient } = require("mongodb");
+
+let singleton;
+
+module.exports = async function connect() {
+  if (singleton) {
+    return singleton;
+  }
+  const client = new MongoClient(process.env.MONGO_URL);
+  await client.connect();
+
+  singleton = client.db(process.env.MONGO_DATABASE);
+  return singleton;
+}
